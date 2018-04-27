667a8576bbb7b6eb603e636a238e2954434396db
Jetoxixoxorix <technoamfa13@gmail.com>
2018-04-27 20:09:29 +0200

    setting limit of content in messages, statuses and comments to 3000 signs

--- src/main/java/com/socialportal/socialportal/models/Message.java
+++ src/main/java/com/socialportal/socialportal/models/Message.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -21,6 +22,7 @@ public class Message {
     private Long id;
 
     @NotNull
+    @Size(max = 3000)
     private String content;
 
     @NotNull
