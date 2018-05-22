edf0af83fee31663977c4d0764fe0887aa376592
Jetoxixoxorix <technoamfa13@gmail.com>
2018-05-22 18:42:55 +0200

    added groups to be created on start of app

--- src/main/java/com/socialportal/socialportal/ExampleDatabase.java
+++ src/main/java/com/socialportal/socialportal/ExampleDatabase.java
@@ -1,13 +1,16 @@
 package com.socialportal.socialportal;
 
+import com.socialportal.socialportal.models.Collective;
 import com.socialportal.socialportal.models.User;
 import com.socialportal.socialportal.models.UserComment;
 import com.socialportal.socialportal.models.UserStatus;
+import com.socialportal.socialportal.services.CollectiveManager;
 import com.socialportal.socialportal.services.ICommentManager;
 import com.socialportal.socialportal.services.IStatusManager;
 import com.socialportal.socialportal.services.IUserManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.provisioning.GroupManager;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -16,14 +19,18 @@ import java.util.LinkedList;
 @Component
 public class ExampleDatabase implements CommandLineRunner {
 
-    @Autowired
-    IUserManager userManager;
+    private IUserManager userManager;
+    private IStatusManager statusManager;
+    private ICommentManager commentManager;
+    private CollectiveManager collectiveManager;
 
     @Autowired
-    IStatusManager statusManager;
-
-    @Autowired
-    ICommentManager commentManager;
+    public ExampleDatabase(IUserManager userManager, IStatusManager statusManager, ICommentManager commentManager, CollectiveManager collectiveManager) {
+        this.userManager = userManager;
+        this.statusManager = statusManager;
+        this.commentManager = commentManager;
+        this.collectiveManager = collectiveManager;
+    }
 
     @Override
     public void run(String... args) throws Exception {
@@ -32,8 +39,8 @@ public class ExampleDatabase implements CommandLineRunner {
         users.add(new User("Piotr", "Prox", "qwerty", "prosty1@gmail.com"));
         users.add(new User("Magda", "Jet", "qwerty", "prosty2@gmail.com"));
         users.add(new User("Jan", "Niezbedny", "qwerty", "prosty3@gmail.com"));
-        users.add( new User("Jan", "Zbedny", "qwerty", "prosty4@gmail.com"));
-        users.add( new User("Piotr", "Kox", "qwerty", "prosty5@gmail.com"));
+        users.add(new User("Jan", "Zbedny", "qwerty", "prosty4@gmail.com"));
+        users.add(new User("Piotr", "Kox", "qwerty", "prosty5@gmail.com"));
 
         for(User user : users){
             userManager.registerUser(user);
@@ -62,6 +69,17 @@ public class ExampleDatabase implements CommandLineRunner {
 
         for(UserComment comment: comments){
             commentManager.addNewComment(comment);
+        }
+
+        LinkedList<Collective> groups = new LinkedList<>();
+        groups.add(new Collective("First Group", "Lorem Ipsum"));
+        groups.add(new Collective("Second Group", "Lorem Ipsum"));
+        groups.add(new Collective("Third Group", "Lorem Ipsum"));
+        groups.add(new Collective("Fourth Group", "Lorem Ipsum"));
+        groups.add(new Collective("Fifth Group", "Lorem Ipsum"));
+
+        for (Collective group: groups){
+            collectiveManager.createGroup(group, users.get(0));
         }
     }
 }
